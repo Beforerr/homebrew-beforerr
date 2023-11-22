ce7e3946e27de4730876c7989ca265c0bcb3c79c
Beforerr <zzj956959688@gmail.com>
2023-11-22 10:16:19 -0800

    fix: format

--- Casks/skteditor.rb
+++ Casks/skteditor.rb
@@ -1,15 +1,15 @@
 cask "skteditor" do
-    version "1.3.6"
-    arch = Hardware::CPU.arch.to_s
+  version "1.3.6"
+  arch = Hardware::CPU.arch.to_s
 
-    if arch == "arm64" then arch = "aarch64" end
-    url "https://spdf.gsfc.nasa.gov/skteditor/skteditor-#{arch}-#{version}.dmg"
-  
-    name "skteditor"
-    desc "ISTP CDF Skeleton Editor"
-    homepage "https://spdf.gsfc.nasa.gov/skteditor"
-  
-    app "skteditor.app"
-  
-    uninstall quit: "gsfc.spdf.edit.guis"
-  end
+  if arch == "arm64" then arch = "aarch64" end
+  url "https://spdf.gsfc.nasa.gov/skteditor/skteditor-#{arch}-#{version}.dmg"
+
+  name "skteditor"
+  desc "ISTP CDF Skeleton Editor"
+  homepage "https://spdf.gsfc.nasa.gov/skteditor"
+
+  app "skteditor.app"
+
+  uninstall quit: "gsfc.spdf.edit.guis"
+end
