3bfea1c336111f2035f45845eef316060fa081a6
Beforerr <zzj956959688@gmail.com>
2024-01-27 09:08:18 -0800

    rename

--- Casks/ssc-4d-orbit-viewer.rb
+++ Casks/ssc-4d-orbit-viewer.rb
@@ -1,9 +1,9 @@
-cask "ssc_4d_orbit_viewer" do
+cask "ssc-4d-orbit-viewer" do
     version "4.2.8"
     # sha256 "b60e7d20527f594bed6f88d665ece5ca04a62894f6a76022a1c8bb36f045a493"
   
     url "https://sscweb.gsfc.nasa.gov/tipsod/4D_Orbit_Viewer.dmg"
-    name "ssc_4d_orbit_viewer"
+    name "ssc-4d-orbit-viewer"
     desc "SSC 4D Orbit Viewer"
     homepage "https://sscweb.gsfc.nasa.gov/tipsod/"
   
