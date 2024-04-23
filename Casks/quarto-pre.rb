5aaa42f04b2b387be703bc88a7091325c04ddc84
Beforerr <58776897+Beforerr@users.noreply.github.com>
2024-04-22 17:22:12 -0700

    Update quarto-pre.rb

--- Casks/quarto-pre.rb
+++ Casks/quarto-pre.rb
@@ -1,6 +1,6 @@
 cask "quarto-pre" do
-    version "1.4.519"
-    sha256 "62b0248b9143667b77e2412573ec831fe93202fccadcb20da0cdcb6f41223e6f"
+    version "1.5.31"
+    sha256 "255918ee358d013c650c28cd257b9a061dcb4572b465c621e4ef8e32211e9468"
   
     url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-macos.pkg",
         verified: "github.com/quarto-dev/quarto-cli/"
