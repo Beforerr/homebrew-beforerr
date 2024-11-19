4bd0963f2d9c9daa548daeebcee4056ccb49abb0
Beforerr <zzj956959688@gmail.com>
2024-11-19 00:01:57 -0800

    Update quarto-pre.rb to version 1.6.36 with new sha256 checksum

--- Casks/quarto-pre.rb
+++ Casks/quarto-pre.rb
@@ -1,6 +1,6 @@
 cask "quarto-pre" do
-    version "1.6.29"
-    sha256 "6f67ae586cfe1ef09e0a736cd608eadac20c0c277d97636c8d965b561723c6bf"
+    version "1.6.36"
+    sha256 "a9661eb6535d0ddede3b172df5545b1f08f59381863298b6e70ad1f9755bce08"
   
     url "https://github.com/quarto-dev/quarto-cli/releases/download/v#{version}/quarto-#{version}-macos.pkg",
         verified: "github.com/quarto-dev/quarto-cli/"
